e3ad5dd86bd5777c78802ca0005164a96976bd90
Philip Whitfield <public@underdoeg.com>
2011-11-16 19:35:47 +0100

    working on views

--- cppdoc/views.py
+++ cppdoc/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from django.template.response import TemplateResponse
+from cppdoc.models import *
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+def index(request):
+    return listClasses(request)
+
+def listClasses(request):
+    c = {}
+    c["classes"] = dClass.objects.all().filter(visible=True)
+    t = loader.get_template('classList.html')
+    rc = RequestContext(request, c)
+    return HttpResponse(t.render(rc),
+    content_type="application/xhtml+xml")
--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ofDoc.views.home', name='home'),
+    url(r'^$', 'cppdoc.views.listClasses', name='index'),
     # url(r'^ofDoc/', include('ofDoc.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
